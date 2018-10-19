ba4ffb02860f3441aba25954f825a1aa251468b3
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-10-18 23:21:53 -0400

    basic cleaning

--- Places/ViewController.swift
+++ Places/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  Places
-//
-//  Created by Aleksey on 1018..18.
-//  Copyright © 2018 Aleksey. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
 }
 
